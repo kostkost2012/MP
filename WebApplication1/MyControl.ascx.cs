391f55eed8b686851d53d53e2d8a85ac159805ee
fffffffftt <@>
2018-10-28 19:15:06 +0300

    Poll control start

--- WebApplication1/MyControl.ascx.cs
+++ WebApplication1/MyControl.ascx.cs
@@ -4,14 +4,41 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using System.Data.SqlClient;
 
 namespace WebApplication1
 {
     public partial class MyControl : System.Web.UI.UserControl
     {
+        TextBox text1 = new TextBox();
         protected void Page_Load(object sender, EventArgs e)
         {
+            text1.ID = "text1";
+            this.Controls.Add(text1);
+        }
+    }
 
+    public class Question
+    {
+        int ID;
+        int type = 0;
+        String text;
+        List<String> answers;
+
+
+        public Question(int id, SqlConnection conn)
+        {
+            this.ID = id;
+
+        }
+
+
+
+        public bool Validate(List<String> answ)
+        {
+            if (answ.Intersect(answers).Count() == answers.Intersect(answ).Count())
+                return true;
+            return false;
         }
     }
 }
